7e999f7418f3f806980ed127c34a8a2f91adf17d
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-02-16 03:15:17 +0000

    changes3

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,13 +5,8 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        char empty = ' ';
-        int counter = 1;
-        for(int j=0; j<in.length(); j++) {
-            if(in.charAt(j)==empty) {
-                counter++;
-            }
-        }
-        return counter;
+        String empty = " ";
+        String counter = in.replaceAll(empty, "");
+        return in.length()-counter.length()+1;
     }
 }
